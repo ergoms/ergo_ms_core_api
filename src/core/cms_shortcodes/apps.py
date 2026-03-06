38d9dd95d96259247c74c81ac3a13051658d4f35
DohaoSTR <muzalevskij.evgenij@mail.ru>
2026-03-06 15:35:40 +0300

    feat: добавлена миграция для обновления источника модуля шорткодов с 'core/shortcodes' на 'cms_shortcodes', удалены устаревшие файлы модуля шорткодов

--- src/core/cms_shortcodes/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class CmsShortcodesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'src.core.cms_shortcodes'
-    label = 'cms_shortcodes'
