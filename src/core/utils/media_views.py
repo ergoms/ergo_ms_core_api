1ccf84b332a4cecb4a2899d87fae053a3525a16a
DohaoSTR <muzalevskij.evgenij@mail.ru>
2026-08-10 18:40:15 +0300

    feat: Добавлена поддержка квоты для загрузки медиафайлов с учетом прав администратора.

--- src/core/utils/media_views.py
+++ src/core/utils/media_views.py
@@ -2,6 +2,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from src.core.cms.adp.services.permissions import PermissionService
 from src.core.utils.media_signing import get_upload_info
 
 
@@ -14,6 +15,7 @@ class MediaUploadTokenView(APIView):
     Response: {upload_url, token}
 
     Параметры проверяются на сервере (см. media_upload_validation).
+    Класс квоты частоты: admin для глобального администратора, иначе user.
     """
     permission_classes = [IsAuthenticated]
 
@@ -21,11 +23,13 @@ class MediaUploadTokenView(APIView):
         target_dir = request.data.get('target_dir', '')
         max_size = request.data.get('max_size')
         allowed_types = request.data.get('allowed_types')
+        quota = 'admin' if PermissionService.is_admin(request.user) else 'user'
 
         upload_info = get_upload_info(
             user_id=request.user.id,
             target_dir=target_dir,
             max_size=max_size,
             allowed_types=allowed_types,
+            quota=quota,
         )
         return Response(upload_info)
